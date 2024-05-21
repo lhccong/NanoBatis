2ebb5acc7601069a56b6d9bf7897c0c6ee58c44a
lhccong <liuhuaicong@zhut.cn>
2024-05-21 11:09:49 +0800

    feat:🚀 新增映射器注册机

---
+++ src/main/java/com/cong/nanobatis/binding/MapperRegistry.java
@@ -0,0 +1,58 @@
+package com.cong.nanobatis.binding;
+
+import cn.hutool.core.lang.ClassScanner;
+import com.cong.nanobatis.exception.NanobatisException;
+import com.cong.nanobatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 映射器注册机
+ *
+ * @author cong
+ * @date 2024/05/21
+ */
+public class MapperRegistry {
+
+    /**
+     * 将已添加的映射器代理加入到 HashMap
+     */
+    private final Map<Class<?>, MapperProxyFactory<?>> knownMappers = new HashMap<>();
+
+    public <T> T getMapper(Class<T> type, SqlSession sqlSession) {
+        final MapperProxyFactory<T> mapperProxyFactory = (MapperProxyFactory<T>) knownMappers.get(type);
+        if (mapperProxyFactory == null) {
+            throw new NanobatisException("Type " + type + " is not known to the MapperRegistry.");
+        }
+        try {
+            return mapperProxyFactory.newInstance(sqlSession);
+        } catch (Exception e) {
+            throw new NanobatisException("Error getting mapper instance. Cause: " + e, e);
+        }
+    }
+
+    public <T> void addMapper(Class<T> type) {
+        /* Mapper 必须是接口才会注册 */
+        if (type.isInterface()) {
+            if (hasMapper(type)) {
+                // 如果重复添加了，报错
+                throw new NanobatisException("Type " + type + " is already known to the MapperRegistry.");
+            }
+            // 注册映射器代理工厂
+            knownMappers.put(type, new MapperProxyFactory<>(type));
+        }
+    }
+
+    public <T> boolean hasMapper(Class<T> type) {
+        return knownMappers.containsKey(type);
+    }
+
+    public void addMappers(String packageName) {
+        Set<Class<?>> mapperSet = ClassScanner.scanPackage(packageName);
+        for (Class<?> mapperClass : mapperSet) {
+            addMapper(mapperClass);
+        }
+    }
+}
