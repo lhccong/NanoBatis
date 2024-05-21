421fcb56cec833f25472bd4f89e8d68e4e2997ce
lhccong <liuhuaicong@zhut.cn>
2024-05-21 16:34:23 +0800

    feat:🚀 新增映射器代理缓存 MapperMethod 并使用 execute 封装方法

--- src/main/java/com/cong/nanobatis/binding/MapperProxy.java
+++ src/main/java/com/cong/nanobatis/binding/MapperProxy.java
@@ -22,9 +22,12 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
 
     private final Class<T> mapperInterface;
 
-    public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface) {
+    private final Map<Method, MapperMethod> methodCache;
+
+    public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethod> methodCache) {
         this.sqlSession = sqlSession;
         this.mapperInterface = mapperInterface;
+        this.methodCache = methodCache;
     }
 
     @Override
@@ -33,8 +36,20 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
         if (Object.class.equals(method.getDeclaringClass())) {
             return method.invoke(this, args);
         } else {
-            //获取接口名称+方法名称作为key
-            return sqlSession.selectOne(method.getName(), args);
+            final MapperMethod mapperMethod = cachedMapperMethod(method);
+            return mapperMethod.execute(sqlSession, args);
         }
+    }
+    /**
+     * 去缓存中找MapperMethod
+     */
+    private MapperMethod cachedMapperMethod(Method method) {
+        MapperMethod mapperMethod = methodCache.get(method);
+        if (mapperMethod == null) {
+            //找不到才去new
+            mapperMethod = new MapperMethod(mapperInterface, method, sqlSession.getConfiguration());
+            methodCache.put(method, mapperMethod);
+        }
+        return mapperMethod;
     }
 }
