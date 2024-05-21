bdc6e4f329bf193f238d75511a6d8399d6ca8009
lhccong <liuhuaicong@zhut.cn>
2024-05-21 14:56:28 +0800

    fix:✅ 修改默认 SqlSession 实现类引入 Configuration 封装 MapperRegistry

--- src/main/java/com/cong/nanobatis/session/defaults/DefaultSqlSession.java
+++ src/main/java/com/cong/nanobatis/session/defaults/DefaultSqlSession.java
@@ -1,6 +1,7 @@
 package com.cong.nanobatis.session.defaults;
 
-import com.cong.nanobatis.binding.MapperRegistry;
+import com.cong.nanobatis.mapping.MappedStatement;
+import com.cong.nanobatis.session.Configuration;
 import com.cong.nanobatis.session.SqlSession;
 
 /**
@@ -14,10 +15,10 @@ public class DefaultSqlSession implements SqlSession {
     /**
      * 映射器注册机
      */
-    private final MapperRegistry mapperRegistry;
+    private final Configuration configuration;
 
-    public DefaultSqlSession(MapperRegistry mapperRegistry) {
-        this.mapperRegistry = mapperRegistry;
+    public DefaultSqlSession(Configuration configuration) {
+        this.configuration = configuration;
     }
 
     @Override
@@ -27,12 +28,18 @@ public class DefaultSqlSession implements SqlSession {
 
     @Override
     public <T> T selectOne(String statement, Object parameter) {
-        return (T) ("你被代理了！" + "方法：" + statement + " 入参：" + parameter);
+        MappedStatement mappedStatement = configuration.getMappedStatement(statement);
+        return (T) ("你被代理了！" + "\n方法：" + statement + "\n入参：" + parameter + "\n待执行SQL：" + mappedStatement.getSql());
     }
 
     @Override
     public <T> T getMapper(Class<T> type) {
-        return mapperRegistry.getMapper(type, this);
+        return configuration.getMapper(type, this);
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return configuration;
     }
 
 }
