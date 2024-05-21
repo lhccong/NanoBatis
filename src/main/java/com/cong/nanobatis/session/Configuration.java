168c92f5028aeac5aa503fb91defaadf00bf76e1
lhccong <liuhuaicong@zhut.cn>
2024-05-21 14:57:19 +0800

    feat:🚀 新增配置项 Configuration 封装 MapperRegistry

---
+++ src/main/java/com/cong/nanobatis/session/Configuration.java
@@ -0,0 +1,52 @@
+package com.cong.nanobatis.session;
+
+
+import com.cong.nanobatis.binding.MapperRegistry;
+import com.cong.nanobatis.mapping.MappedStatement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 配置项
+ *
+ * @author cong
+ * @date 2022/04/06
+ */
+public class Configuration {
+
+    /**
+     * 映射注册机
+     */
+    protected MapperRegistry mapperRegistry = new MapperRegistry(this);
+
+    /**
+     * 映射的语句，存在Map里
+     */
+    protected final Map<String, MappedStatement> mappedStatements = new HashMap<>();
+
+    public void addMappers(String packageName) {
+        mapperRegistry.addMappers(packageName);
+    }
+
+    public <T> void addMapper(Class<T> type) {
+        mapperRegistry.addMapper(type);
+    }
+
+    public <T> T getMapper(Class<T> type, SqlSession sqlSession) {
+        return mapperRegistry.getMapper(type, sqlSession);
+    }
+
+    public boolean hasMapper(Class<?> type) {
+        return mapperRegistry.hasMapper(type);
+    }
+
+    public void addMappedStatement(MappedStatement ms) {
+        mappedStatements.put(ms.getId(), ms);
+    }
+
+    public MappedStatement getMappedStatement(String id) {
+        return mappedStatements.get(id);
+    }
+
+}
