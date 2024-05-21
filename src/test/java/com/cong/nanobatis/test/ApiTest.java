4cf968f66e47ba6af7171c5863ef54630a127e7d
lhccong <liuhuaicong@zhut.cn>
2024-05-21 16:38:15 +0800

    feat:🚀 新增单元测试

--- src/test/java/com/cong/nanobatis/test/ApiTest.java
+++ src/test/java/com/cong/nanobatis/test/ApiTest.java
@@ -1,15 +1,17 @@
 package com.cong.nanobatis.test;
 
-import com.cong.nanobatis.binding.MapperRegistry;
+import com.cong.nanobatis.io.Resources;
 import com.cong.nanobatis.session.SqlSession;
 import com.cong.nanobatis.session.SqlSessionFactory;
-import com.cong.nanobatis.session.defaults.DefaultSqlSessionFactory;
+import com.cong.nanobatis.session.SqlSessionFactoryBuilder;
 import com.cong.nanobatis.test.dao.UserDao;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.lang.reflect.Proxy;
 
 
@@ -25,22 +27,19 @@ public class ApiTest {
     private final Logger logger = LoggerFactory.getLogger(ApiTest.class);
 
     @Test
-    public void test_MapperProxyFactory() {
-        // 1. 注册 Mapper
-        MapperRegistry registry = new MapperRegistry();
-        registry.addMappers("com.cong.nanobatis.test.dao");
-
-        // 2. 从 SqlSession 工厂获取 Session
-        SqlSessionFactory sqlSessionFactory = new DefaultSqlSessionFactory(registry);
+    public void test_MapperProxyFactory() throws IOException {
+        // 1. 从SqlSessionFactory中获取SqlSession
+        Reader reader = Resources.getResourceAsReader("mybatis-config-datasource.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
-        // 3. 获取映射器对象
+        // 2. 获取映射器对象
         UserDao userDao = sqlSession.getMapper(UserDao.class);
 
-        // 4. 测试验证
+        // 3. 测试验证
         String res = userDao.queryUserName("10001");
-        logger.info("测试结果：{}", res);
         Assert.assertNotNull(res);
+        logger.info("测试结果：{}", res);
     }
 
     @Test
