9ad02826da96c852dba3ca3be29785602b0eaaf4
lhccong <liuhuaicong@zhut.cn>
2024-05-21 14:58:01 +0800

    feat:🚀 新增映射语句类，用于封装映射语句相关属性

---
+++ src/main/java/com/cong/nanobatis/mapping/MappedStatement.java
@@ -0,0 +1,107 @@
+package com.cong.nanobatis.mapping;
+
+import com.cong.nanobatis.session.Configuration;
+
+import java.util.Map;
+
+/**
+ * 映射语句类
+ *
+ * @author cong
+ * @date 2024/05/21
+ */
+public class MappedStatement {
+
+    private Configuration configuration;
+    private String id;
+    private SqlCommandType sqlCommandType;
+
+    private String sql;
+    private String parameterType;
+    private String resultType;
+    private Map<Integer, String> parameter;
+
+    MappedStatement() {
+        // constructor disabled
+    }
+    /**
+     * 建造者
+     */
+    public static class Builder {
+
+        private final MappedStatement mappedStatement = new MappedStatement();
+
+        public Builder(Configuration configuration, String id, SqlCommandType sqlCommandType, String parameterType, String resultType, String sql, Map<Integer, String> parameter) {
+            mappedStatement.configuration = configuration;
+            mappedStatement.id = id;
+            mappedStatement.sqlCommandType = sqlCommandType;
+            mappedStatement.parameterType = parameterType;
+            mappedStatement.resultType = resultType;
+            mappedStatement.sql = sql;
+            mappedStatement.parameter = parameter;
+        }
+
+        public MappedStatement build() {
+            assert mappedStatement.configuration != null;
+            assert mappedStatement.id != null;
+            return mappedStatement;
+        }
+
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(String resultType) {
+        this.resultType = resultType;
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public SqlCommandType getSqlCommandType() {
+        return sqlCommandType;
+    }
+
+    public void setSqlCommandType(SqlCommandType sqlCommandType) {
+        this.sqlCommandType = sqlCommandType;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public String getParameterType() {
+        return parameterType;
+    }
+
+    public void setParameterType(String parameterType) {
+        this.parameterType = parameterType;
+    }
+
+    public Map<Integer, String> getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(Map<Integer, String> parameter) {
+        this.parameter = parameter;
+    }
+}
