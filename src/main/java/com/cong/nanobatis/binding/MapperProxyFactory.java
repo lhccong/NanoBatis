9cb0a7c0dbce6344793b5e5b34d188b33046947f
lhccong <liuhuaicong@zhut.cn>
2024-05-21 16:36:17 +0800

    feat:🚀 新增 Mapper 代理工厂的 methodCache 缓存

--- src/main/java/com/cong/nanobatis/binding/MapperProxyFactory.java
+++ src/main/java/com/cong/nanobatis/binding/MapperProxyFactory.java
@@ -2,7 +2,10 @@ package com.cong.nanobatis.binding;
 
 import com.cong.nanobatis.session.SqlSession;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Mapper 代理工厂
@@ -14,12 +17,18 @@ public class MapperProxyFactory<T> {
 
     private final Class<T> mapperInterface;
 
+    private final Map<Method, MapperMethod> methodCache = new ConcurrentHashMap<Method, MapperMethod>();
+
     public MapperProxyFactory(Class<T> mapperInterface) {
         this.mapperInterface = mapperInterface;
     }
 
+    public Map<Method, MapperMethod> getMethodCache() {
+        return methodCache;
+    }
+
     public T newInstance(SqlSession sqlSession) {
-        final MapperProxy<T> mapperProxy = new MapperProxy<>(sqlSession, mapperInterface);
+        final MapperProxy<T> mapperProxy = new MapperProxy<>(sqlSession, mapperInterface, methodCache);
         return (T) Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[]{mapperInterface}, mapperProxy);
     }
 
