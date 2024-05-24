c38588eb164a6bffe36846406488c182cf333e9b
lhccong <liuhuaicong@zhut.cn>
2024-05-24 16:01:27 +0800

    feat:🚀 配置类新增环境、类型别名注册机

--- src/main/java/com/cong/nanobatis/session/Configuration.java
+++ src/main/java/com/cong/nanobatis/session/Configuration.java
@@ -2,7 +2,11 @@ package com.cong.nanobatis.session;
 
 
 import com.cong.nanobatis.binding.MapperRegistry;
+import com.cong.nanobatis.datasource.druid.DruidDataSourceFactory;
+import com.cong.nanobatis.mapping.Environment;
 import com.cong.nanobatis.mapping.MappedStatement;
+import com.cong.nanobatis.transaction.jdbc.JdbcTransactionFactory;
+import com.cong.nanobatis.type.TypeAliasRegistry;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +18,10 @@ import java.util.Map;
  * @date 2022/04/06
  */
 public class Configuration {
+    /**
+     * 环境
+     */
+    protected Environment environment;
 
     /**
      * 映射注册机
@@ -24,6 +32,17 @@ public class Configuration {
      * 映射的语句，存在Map里
      */
     protected final Map<String, MappedStatement> mappedStatements = new HashMap<>();
+
+
+    /**
+     * 类型别名注册机
+     */
+    protected final TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+
+    public Configuration() {
+        typeAliasRegistry.registerAlias("JDBC", JdbcTransactionFactory.class);
+        typeAliasRegistry.registerAlias("DRUID", DruidDataSourceFactory.class);
+    }
 
     public void addMappers(String packageName) {
         mapperRegistry.addMappers(packageName);
@@ -47,6 +66,18 @@ public class Configuration {
 
     public MappedStatement getMappedStatement(String id) {
         return mappedStatements.get(id);
+    }
+
+    public TypeAliasRegistry getTypeAliasRegistry() {
+        return typeAliasRegistry;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
 }
