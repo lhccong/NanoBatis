82e54949d735a3e471c91ba4aef46a1da61ebaaa
lhccong <liuhuaicong@zhut.cn>
2024-05-24 17:11:08 +0800

    feat:🚀 构建器携带基本类型

--- src/main/java/com/cong/nanobatis/builder/BaseBuilder.java
+++ src/main/java/com/cong/nanobatis/builder/BaseBuilder.java
@@ -2,6 +2,7 @@ package com.cong.nanobatis.builder;
 
 
 import com.cong.nanobatis.session.Configuration;
+import com.cong.nanobatis.type.TypeAliasRegistry;
 
 
 /**
@@ -13,9 +14,11 @@ import com.cong.nanobatis.session.Configuration;
 public abstract class BaseBuilder {
 
     protected final Configuration configuration;
+    protected final TypeAliasRegistry typeAliasRegistry;
 
     protected BaseBuilder(Configuration configuration) {
         this.configuration = configuration;
+        this.typeAliasRegistry = this.configuration.getTypeAliasRegistry();
     }
 
     public Configuration getConfiguration() {
