a6b39c8ff8e0102e5fbfb36f8a7907f4973b81ef
lhccong <liuhuaicong@zhut.cn>
2024-05-24 15:33:04 +0800

    feat:🚀 新增环境变量配置功能

---
+++ src/main/java/com/cong/nanobatis/mapping/Environment.java
@@ -0,0 +1,68 @@
+package com.cong.nanobatis.mapping;
+
+import com.cong.nanobatis.transaction.TransactionFactory;
+
+import javax.sql.DataSource;
+
+/**
+ * 环境
+ *
+ * @author cong
+ * @date 2024/05/24
+ */
+public class Environment {
+    // 环境id
+    private final String id;
+    // 事务工厂
+    private final TransactionFactory transactionFactory;
+    // 数据源
+    private final DataSource dataSource;
+
+    public Environment(String id, TransactionFactory transactionFactory, DataSource dataSource) {
+        this.id = id;
+        this.transactionFactory = transactionFactory;
+        this.dataSource = dataSource;
+    }
+
+    public static class Builder {
+
+        private final String id;
+        private TransactionFactory transactionFactory;
+        private DataSource dataSource;
+
+        public Builder(String id) {
+            this.id = id;
+        }
+
+        public Builder transactionFactory(TransactionFactory transactionFactory) {
+            this.transactionFactory = transactionFactory;
+            return this;
+        }
+
+        public Builder dataSource(DataSource dataSource) {
+            this.dataSource = dataSource;
+            return this;
+        }
+
+        public String id() {
+            return this.id;
+        }
+
+        public Environment build() {
+            return new Environment(this.id, this.transactionFactory, this.dataSource);
+        }
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TransactionFactory getTransactionFactory() {
+        return transactionFactory;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+}
